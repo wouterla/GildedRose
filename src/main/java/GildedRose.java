8adc45897940d16b492e8c773403363a399d1d1e
OHarvey <oliver.harvey@adrman.co.uk>
2017-04-12 18:59:08 +0100

    Reverted Main Class

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public class GildedRose {
 
-    private static List<Item> items = null;
-
     /**
      * @param args
      */
@@ -12,7 +10,7 @@ public class GildedRose {
 
         System.out.println("OMGHAI!");
 
-        items = new ArrayList<Item>();
+        List<Item> items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
         items.add(new Item("Elixir of the Mongoose", 5, 7));
@@ -20,110 +18,59 @@ public class GildedRose {
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
+        updateQuality(items);
     }
 
-    public static void updateQuality() {
+    public static void updateQuality(List<Item> items) {
         for (int i = 0; i < items.size(); i++) {
-            changeQuality(i);
-            changeSellIn(i);
-        }
-    }
+            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
+                if (items.get(i).getQuality() > 0) {
+                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
+                        items.get(i).setQuality(items.get(i).getQuality() - 1);
+                    }
+                }
+            } else {
+                if (items.get(i).getQuality() < 50) {
+                    items.get(i).setQuality(items.get(i).getQuality() + 1);
 
-    private static void changeSellIn(int i) {
-        reduceNonLegendaryItemSellIn(i);
-        if (hasSellInExpired(i) && isNotAgedBrie(i)) {
-            computeExpiredItemQuality(i);
-        } else {
-            increaseValueOfBrie(i);
-        }
-    }
+                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
+                        if (items.get(i).getSellIn() < 11) {
+                            if (items.get(i).getQuality() < 50) {
+                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                            }
+                        }
 
-    private static boolean hasSellInExpired(int i) {
-        return items.get(i).getSellIn() < 0;
-    }
+                        if (items.get(i).getSellIn() < 6) {
+                            if (items.get(i).getQuality() < 50) {
+                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                            }
+                        }
+                    }
+                }
+            }
 
-    private static void increaseValueOfBrie(int i) {
-        if (isLessThenMaxQuality(i)) {
-            increaseQuality(i);
-        }
-    }
+            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
+                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+            }
 
-    private static void computeExpiredItemQuality(int i) {
-        if (isBackstageTickets(i)) {
-            setItemQualityToZero(i);
-        } else {
-            devalueStandardItem(i);
-        }
-    }
-
-    private static void changeQuality(int i) {
-        if (isNotAgedBrie(i) && !isBackstageTickets(i)) {
-            devalueStandardItem(i);
-        } else {
-            if (isLessThenMaxQuality(i)) {
-                increaseQuality(i);
-                if (isBackstageTickets(i)) {
-                    increaseTimelyItemsQuality(i);
+            if (items.get(i).getSellIn() < 0) {
+                if (!"Aged Brie".equals(items.get(i).getName())) {
+                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
+                        if (items.get(i).getQuality() > 0) {
+                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
+                                items.get(i).setQuality(items.get(i).getQuality() - 1);
+                            }
+                        }
+                    } else {
+                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
+                    }
+                } else {
+                    if (items.get(i).getQuality() < 50) {
+                        items.get(i).setQuality(items.get(i).getQuality() + 1);
+                    }
                 }
             }
         }
     }
 
-    private static void increaseTimelyItemsQuality(int i) {
-        if (items.get(i).getSellIn() < 11) {
-            increaseQuality(i);
-        }
-        if (items.get(i).getSellIn() < 6) {
-            increaseQuality(i);
-        }
-    }
-
-    private static void reduceNonLegendaryItemSellIn(int i) {
-        if (notLegendaryItem(i)) {
-            reduceSellIn(i);
-        }
-    }
-
-    private static void devalueStandardItem(int i) {
-        if (isQualityGreaterThanMin(i) && notLegendaryItem(i)) {
-            reduceQuality(i);
-        }
-    }
-
-    private static void setItemQualityToZero(int i) {
-        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-    }
-
-    private static boolean isNotAgedBrie(int i) {
-        return !"Aged Brie".equals(items.get(i).getName());
-    }
-
-    private static boolean isBackstageTickets(int i) {
-        return "Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName());
-    }
-
-    private static boolean notLegendaryItem(int i) {
-        return !"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName());
-    }
-
-    private static boolean isLessThenMaxQuality(int i) {
-        return items.get(i).getQuality() < 50;
-    }
-
-    private static boolean isQualityGreaterThanMin(int i) {
-        return items.get(i).getQuality() > 0;
-    }
-
-    private static void increaseQuality(int i) {
-        items.get(i).setQuality(items.get(i).getQuality() + 1);
-    }
-
-    private static void reduceQuality(int i) {
-        items.get(i).setQuality(items.get(i).getQuality() - 1);
-    }
-
-    private static void reduceSellIn(int i) {
-        items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-    }
 }
